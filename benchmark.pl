bb8130f134ae0dc2b20ff1d8874eb7f1e6ed7593
Martin Popel <popel@ufal.mff.cuni.cz>
2015-12-18 03:00:02 +0100

    more benchmark tasks, use Time::HiRes

--- benchmark.pl
+++ benchmark.pl
@@ -2,15 +2,16 @@
 use strict;
 use warnings;
 use Text::Table;
+use Time::HiRes qw (time);
 
-my @header = qw(init load save TOTAL MAXMEM);
+my @header = qw(TOTAL MAXMEM init load save iter iterF read write rehang remove add reorder);
 my %in_header = map {$_ => 1} @header;
 
 # Don't use any shell metacharacters (e.g. redirections) in the commands below.
 # That would result in executing shell subprocess and the memory consumption
 # would be of that subprocess not of the main process we want to evaluate.
 my @experiments = (
-    [dummy              => './dummy.pl'],
+    #[dummy              => './dummy.pl'],
     [old_Treex_ro_dev   => './old-treex.pl data/UD_Romanian/ro-ud-dev.conllu /tmp/out.conllu'],
     [old_Treex_ro_train => './old-treex.pl data/UD_Romanian/ro-ud-train.conllu /tmp/out.conllu'],
 );
@@ -31,11 +32,12 @@ sub run {
         $mem = sprintf "%.3f", $mem/1024;
         $maxmem = $mem if $mem > $maxmem;
         chomp;
-        $t{$_} = $now - $last;
-        printf STDERR "%20s %5ds %8sMiB\n", $_, $now-$last, $mem;
+        my $time = sprintf '%.3f', $now - $last;
+        $t{$_} = $time;
+        printf STDERR "%20s %9ss %10sMiB\n", $_, $time, $mem;
         $last = $now;
     }
-    $t{TOTAL} = time - $start;
+    $t{TOTAL} = sprintf '%.3f', time - $start;
     $t{MAXMEM} = $maxmem;
     return \%t;
 }
--- old-treex.pl
+++ old-treex.pl
@@ -7,12 +7,106 @@ use Treex::Block::Write::CoNLLU;
 use Treex::Core::Log;
 Treex::Core::Log::log_set_error_level('WARN');
 my ($in_conllu, $out_conllu) = @ARGV;
-
 print "init\n";
+
 my $reader = Treex::Block::Read::CoNLLU->new({from=>$in_conllu});
 my $doc = $reader->next_document();
 print "load\n";
+
 my $writer = Treex::Block::Write::CoNLLU->new({to=>$out_conllu});
 $writer->process_document($doc);
 print "save\n";
 
+foreach my $bundle ($doc->get_bundles()){
+    # There is just one tree in each bundle, but let's make the code more general
+    foreach my $tree ($bundle->get_all_trees()){
+        foreach my $node ($tree->get_descendants({ordered=>1})){
+            # no op
+        }
+    }
+}
+print "iter\n";
+
+foreach my $bundle ($doc->get_bundles()){
+    foreach my $tree ($bundle->get_all_trees()){
+        foreach my $node ($tree->get_descendants()){
+            # no op
+        }
+    }
+}
+print "iterF\n";
+
+
+foreach my $bundle ($doc->get_bundles()){
+    foreach my $tree ($bundle->get_all_trees()){
+        foreach my $node ($tree->get_descendants({ordered=>1})){
+            my $form_lemma_tag = $node->form . $node->lemma;
+        }
+    }
+}
+print "read\n";
+
+foreach my $bundle ($doc->get_bundles()){
+    foreach my $tree ($bundle->get_all_trees()){
+        foreach my $node ($tree->get_descendants({ordered=>1})){
+            $node->set_conll_deprel('dep');
+        }
+    }
+}
+print "write\n";
+
+foreach my $bundle ($doc->get_bundles()){
+    foreach my $tree ($bundle->get_all_trees()){
+        my @nodes = $tree->get_descendants({ordered=>1});
+        foreach my $node (@nodes){
+            # rehanging to a random parent may result in cycles, which should result in exception
+            # eval {} is the pure-Perl way of try-catch.
+            # Let's also prevent the TREEX-FATAL messages on stderr
+            local $SIG{__WARN__} = sub {};
+            eval {
+                my $rand_index = int(rand($#nodes+1));
+                $node->set_parent($nodes[$rand_index]);
+            }
+        }
+    }
+}
+print "rehang\n";
+
+foreach my $bundle ($doc->get_bundles()){
+    foreach my $tree ($bundle->get_all_trees()){
+        foreach my $node ($tree->get_descendants({ordered=>1})){
+            $node->remove if rand() < 0.1 && ref $node ne 'Treex::Core::Node::Deleted';
+        }
+    }
+}
+print "remove\n";
+
+foreach my $bundle ($doc->get_bundles()){
+    foreach my $tree ($bundle->get_all_trees()){
+        foreach my $node ($tree->get_descendants({ordered=>1})){
+            if (rand() < 0.1) {
+                $node->create_child({form=>'x', lemma=>'x'})->shift_after_subtree($node);
+            }
+        }
+    }
+}
+print "add\n";
+
+foreach my $bundle ($doc->get_bundles()){
+    foreach my $tree ($bundle->get_all_trees()){
+        my @nodes = $tree->get_descendants({ordered=>1});
+        foreach my $node (@nodes){
+            my $rand_index = int(rand($#nodes+1));
+            if (rand() < 0.1) {
+                # Catch an exception if $nodes[$rand_index] is a descendant of $node
+                local $SIG{__WARN__} = sub {};
+                eval {
+                    $node->shift_after_node($nodes[$rand_index]);
+                }
+            } elsif (rand() < 0.1) {
+                $node->shift_before_subtree($nodes[$rand_index], {without_children=>1});
+            }
+        }
+    }
+}
+print "reorder\n";
