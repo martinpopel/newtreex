97846a73d38aabc038fff7318a1c570986e57380
Zdeněk Žabokrtský <zabokrtsky@ufal.mff.cuni.cz>
2016-05-12 09:36:44 +0200

    - fixing pythonpath - demo output contains the name of the programming languages, so that one can see that all outputs are identical

--- demo/python-demo.sh
+++ demo/python-demo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 export PATH=../python/bin:$PATH
-export PYTHONPATH=../python/lib:$PYTHONPATH
+export PYTHONPATH=../python/:$PYTHONPATH
 
-udapi.py read.Conllu filename=sample.conllu Dummy write.Conllu > transformed.conllu
+udapi.py read.Conllu filename=sample.conllu Dummy write.Conllu > transformed-by-python.conllu
